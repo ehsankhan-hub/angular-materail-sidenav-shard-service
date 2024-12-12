048942a3bf4dbf14d602ef9519d8790abb9bc21e
shradhanjalimishra <shradhanjali.mishra@efi.com>
2024-12-12 21:12:48 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,46 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatToolbarModule,MatSidenavModule,MatIconModule,MatListModule,MatButtonModule,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-sidenav';
+
+  menuItems: any[] =[
+    {
+      icon:'home',
+      label:'Home',
+      route:'home'
+    },
+    {
+      icon:'handshake',
+      label:'Partners',
+      route:'partners'
+    },
+    {
+      icon:'book',
+      label:'Training',
+      route:'training'
+    },
+    {
+      icon:'event',
+      label:'Events',
+      route:'event'
+    },
+    {
+      icon:'help',
+      label:'Support',
+      route:'support'
+    }
+  ];
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PartnersComponent } from './pages/partners/partners.component';
+import { TrainingComponent } from './pages/training/training.component';
+import { EventsComponent } from './pages/events/events.component';
+import { SupportComponent } from './pages/support/support.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        pathMatch:'full',
+        redirectTo:'home'
+    },
+    {
+        path:'home',
+        component: HomeComponent
+    },
+    {
+        path:'partners',
+        component:PartnersComponent
+    },
+    {
+        path:'training',
+        component:TrainingComponent
+    },
+    {
+        path:'event',
+        component:EventsComponent
+    },
+    {
+        path:'support',
+        component:SupportComponent
+    },
+];
